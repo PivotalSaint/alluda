071234118c52f2063f0a00aa348528e3be3ac178
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-21 10:43:34 -0500

    styled format of layout and working on api function

---
+++ public/js/media.js
@@ -0,0 +1,11 @@
+var getVideo = function() {
+    console.log("function was called");
+    fetch("https://www.youtube.com/watch?v=Hc10febKlX8").then(function(response){
+        console.log("response");
+    });
+    console.log("response");
+}
+
+:,(
+
+getVideo();
