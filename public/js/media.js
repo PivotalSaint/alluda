b9bd9e841c51e53023b636c811df685628ca7c6d
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-21 17:56:54 -0500

    added correct script

--- public/js/media.js
+++ public/js/media.js
@@ -1,11 +1,15 @@
-var getVideo = function() {
+const getVideo = function(playVideo) {
+
     console.log("function was called");
     fetch("https://www.youtube.com/watch?v=Hc10febKlX8").then(function(response){
-        console.log("response");
-    });
-    console.log("response");
+        console.log("resp");
+    })
+    .then(
+        playVideo()
+    )
+
 }
 
-:,(
+
 
 getVideo();
