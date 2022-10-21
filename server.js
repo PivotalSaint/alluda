4634bba282a0821525eaf5a092d88ebb54f1c509
PivotalSaint <licensedmt@att.net>
2022-10-20 21:34:12 -0500

    journal log updated

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const express = require('express');
+const express = require("express");
+
+const PORT = process.env.PORT || 3350;
+
+const app = express();
+const apiRoutes = require("../alluda/routes/apiRoutes");
+const htmlRoutes = require("../alluda/routes/htmlRoutes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
