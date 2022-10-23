a8b2fd4c9b0ee359eb9c1ce9bfc38ccb81a792a8
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-23 07:19:43 -0500

    handlerexperss const and link is set up in server

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const path = require('path');
+const exphbs = require('express-handlbars');
 
 const PORT = process.env.PORT || 3350;
 
+const hbs = exphbs.create({});
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
+app.engine('handlebars', hbs.engine);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view engine', 'handlebars');
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
