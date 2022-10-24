791891abe834067fe4a647972aeb2ef8bf894200
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-24 18:21:35 -0500

    added login into user routes

--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
+const bcrypt = require('bcrypt');
 
 //User Models
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 //Tables columns and configuraton
 User.init(
@@ -41,6 +45,20 @@ User.init(
 
     },
     {
+        hooks:{
+          // set up beforeCreate lifecycle "hook" functionality
+          async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+    
+          // set up beforeUpdate lifecycle "hook" functionality
+          async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          }
+        },
+    
         sequelize,
         timestamps: false,
         freezeTableName: true,
--- server.js
+++ server.js
@@ -1,48 +1,34 @@
-// const express = require("express");
-// const path = require('path');
-// const exphbs = require('express-handlbars');
-// const routes = require('./routes');
-// const sequelize = require('./config/connection');
-
-// const PORT = process.env.PORT || 3350;
-
-// const hbs = exphbs.create({});
-// const app = express();
-// const apiRoutes = require("./routes/apiRoutes");
-// const htmlRoutes = require("./routes/htmlRoutes");
-
-// app.engine('handlebars', hbs.engine);
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static("public"));
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(routes);
-// app.set('view engine', 'handlebars');
-// app.use("/api", apiRoutes);
-// app.use("/", htmlRoutes);
-
-
-// sequelize.sync({ force: false }).then(() => {
-
-//   app.listen(PORT, () => console.log(`Now listening`));
-
-// });
-
 const express = require("express");
+const path = require('path');
+const exphbs = require('express-handlebars');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
 
 const PORT = process.env.PORT || 3350;
 
+const hbs = exphbs.create({});
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
+app.engine('handlebars', hbs.engine);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+app.set('view engine', 'handlebars');
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+
+sequelize.sync({ force: false }).then(() => {
+
+  app.listen(PORT, () => console.log(`Now listening`));
+
 });
+
+
+// app.listen(PORT, () => {
+//   console.log(`API server now on port ${PORT}!`);
+// });
