a60033c2f95d026d278ca66f2122dc96fb59f96e
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-30 19:29:14 -0500

    added dashboard page

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+
+
+router.get('/', (req, res) => {
+  res.render('dashboard', { loggedIn: true });
+});
+
+
+module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,3 @@
-async function loginFormHandler(event) {
-  event.preventDefault();
-
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-
-  if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'post',
-      body: JSON.stringify({
-        email,
-        password
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
 async function signupFormHandler(event) {
   event.preventDefault();
 
@@ -45,6 +21,30 @@ async function signupFormHandler(event) {
         console.log('success');
     } else {
         alert(response.statusText);
+    }
+  }
+}
+
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
     }
   }
 }
